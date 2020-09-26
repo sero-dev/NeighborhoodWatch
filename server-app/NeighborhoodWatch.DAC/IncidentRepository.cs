2296ebf47e33c5d06f8f9147dda9998e413e59b9
Sean Rodriguez <sean.rodriguez@outlook.com>
2020-09-26 12:47:31 -0400

    Add repository for CRUD operations on the Incident table

---
+++ server-app/NeighborhoodWatch.DAC/IncidentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using NeighborhoodWatch.DAC.Context;
+using NeighborhoodWatch.DAC.Entity;
+using NeighborhoodWatch.DAC.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeighborhoodWatch.DAC
+{
+    public class IncidentRepository : IIncidentRepository
+    {
+        private readonly IncidentContext _dbContext;
+
+        public IncidentRepository(IncidentContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IQueryable<Incident> GetByType(string type)
+        {
+            return _dbContext.Incident.Where(i => i.Type.ToLower() == type.ToLower());
+        }
+
+        public bool Add(Incident newIncident) 
+        {
+            try
+            {
+                _dbContext.Incident.Add(newIncident);
+                _dbContext.SaveChanges();
+                return true;
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            
+        }
+
+        public IQueryable<Incident> GetAll()
+        {
+            return _dbContext.Incident;
+        }
+    }
+}
