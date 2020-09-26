03111c14e57c06e1711f3444a91138518a4d5208
Sean Rodriguez <sean.rodriguez@outlook.com>
2020-09-26 12:46:19 -0400

    Add entity context and models

---
+++ server-app/NeighborhoodWatch.DAC/Context/IncidentContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using NeighborhoodWatch.DAC.Entity;
+
+namespace NeighborhoodWatch.DAC.Context
+{
+    public partial class IncidentContext : DbContext
+    {
+        public IncidentContext()
+        {
+        }
+
+        public IncidentContext(DbContextOptions<IncidentContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Incident> Incident { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                throw new InvalidOperationException("Database context is not configured");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Incident>(entity =>
+            {
+                entity.ToTable("incident");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Location).HasColumnName("location");
+
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasColumnName("type");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ server-app/NeighborhoodWatch.DAC/Entity/Incident.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using NpgsqlTypes;
+
+namespace NeighborhoodWatch.DAC.Entity
+{
+    public partial class Incident
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public NpgsqlPoint Location { get; set; }
+    }
+}
