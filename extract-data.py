224517b0ae11cd7e88c436f7be1347c5ec773285
Bolshevik35 <hongquan03051997@gmail.com>
2020-09-26 18:34:43 -0700

    produce heatmap

--- extract-data.py
+++ extract-data.py
@@ -6,12 +6,12 @@ import folium
 from folium import plugins
 
 def main():
-	counties_lat_lon = pd.read_csv('../COVID-19_Cases.csv')
+	counties_lat_lon = pd.read_csv('COVID-19_Cases.csv')
 	counties_lat_lon = counties_lat_lon.filter(["Lat", "Long_", "Province_State","Admin2"], axis=1)
 	counties_lat_lon.columns = ['latitude', 'longitude', 'state', 'county']
 	counties_lat_lon = counties_lat_lon[counties_lat_lon['state'] == 'Florida']
 
-	states_cases = pd.read_csv('./live/us-counties.csv')
+	states_cases = pd.read_csv('us-counties.csv')
 	fl = states_cases.filter(['date','county','state', 'cases', 'deaths'], axis=1)
 	fl = fl[fl['state'] == 'Florida']
 	fl = fl.groupby('county').aggregate('sum').reset_index()
